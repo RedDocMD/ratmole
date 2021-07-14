33a5c3d7526ea84da0c369f1a92b871c36011fcf
Deep Majumder <deep.majumder2019@gmail.com>
2021-07-14 14:05:35 +0530

    downloading example ready

--- examples/dependency-discover.rs
+++ examples/dependency-discover.rs
@@ -1,4 +1,4 @@
-use cargo::{sources::SourceConfigMap, Config};
+use cargo::{core::SourceId, sources::SourceConfigMap, Config};
 use colored::*;
 use ratmole::{
     cargo::{download_dependency, parse_cargo},
@@ -25,15 +25,38 @@ fn main() -> Result<(), Error> {
 
     let config = Config::default()?;
     let _lock = config.acquire_package_cache_lock()?;
+    let crates_io_id = SourceId::crates_io(&config)?;
+    let config_map = SourceConfigMap::new(&config)?;
+    let mut crates_io = config_map.load(crates_io_id, &Default::default())?;
+    crates_io.update()?;
 
     let manifest = parse_cargo(crate_root, &config)?;
+    let mut pkgs = Vec::new();
+    for dep in manifest.dependencies() {
+        println!(
+            "{}",
+            format!("Downloading {} ...", dep.name_in_toml()).yellow()
+        );
+        if dep.source_id() == crates_io_id {
+            pkgs.push(download_dependency(dep, &mut crates_io, &config)?);
+        } else {
+            let config_map = SourceConfigMap::new(&config)?;
+            let mut src = config_map.load(dep.source_id(), &Default::default())?;
+            src.update()?;
+            pkgs.push(download_dependency(dep, &mut src, &config)?);
+        }
+        println!(
+            "{}",
+            format!(" ... downloaded {}", dep.name_in_toml()).green()
+        );
+    }
+    for pkg in &pkgs {
+        println!(
+            "{} {}",
+            pkg.name(),
+            pkg.root().as_os_str().to_str().unwrap()
+        );
+    }
 
-    let dep = &manifest.dependencies()[0];
-    let map = SourceConfigMap::new(&config)?;
-    let mut src = map.load(dep.source_id(), &Default::default())?;
-
-    src.update()?;
-    let pkg = download_dependency(dep, &mut src, &config)?;
-    println!("{}", pkg.root().as_os_str().to_str().unwrap());
     Ok(())
 }
