a134841271efaa9c0908d0569958bfeb7844a147
Deep Majumder <deep.majumder2019@gmail.com>
2021-10-05 21:23:28 +0530

    Using libgit instead of explicit git command calls

--- src/stdlib.rs
+++ src/stdlib.rs
@@ -1,10 +1,9 @@
 use std::{
     fs,
     path::{Path as StdPath, PathBuf},
-    process::Command,
 };
 
-use git2::{Repository, Tag};
+use git2::{Commit, FetchOptions, Oid, Repository, Tag};
 
 use crate::error::{Error, Result};
 
@@ -23,144 +22,141 @@ fn init_base_dir<P: AsRef<StdPath>>(path: P) -> Result<()> {
     Ok(())
 }
 
-const REMOTE_URL: &'static str = "https://github.com/rust-lang/rust";
-const BASE_DIRNAME: &'static str = ".ratmole";
-const REPO_DIRNAME: &'static str = "rust-repo";
-const GIT_COMMAND: &'static str = "/usr/bin/git";
-const MAIN_BRANCH: &'static str = "master";
-const REMOTE_REFSPEC: &'static str = "origin/master";
+const REMOTE_URL: &str = "https://github.com/rust-lang/rust";
+const BASE_DIRNAME: &str = ".ratmole";
+const REPO_DIRNAME: &str = "rust-repo";
+const MAIN_BRANCH: &str = "master";
 
-fn repo_clone() -> Result<()> {
-    let mut base_dir =
-        home::home_dir().ok_or_else(|| Error::HomeDirNotFound("home dir not found"))?;
+fn repo_clone() -> Result<Repository> {
+    let mut base_dir = home::home_dir().ok_or(Error::HomeDirNotFound("home dir not found"))?;
     base_dir.push(BASE_DIRNAME);
     init_base_dir(&base_dir)?;
-
-    // Clone directory
-    let clone_output = Command::new(GIT_COMMAND)
-        .current_dir(&base_dir)
-        .args(&["clone", REMOTE_URL, REPO_DIRNAME])
-        .output()
-        .expect("Failed to run git clone");
-    assert!(clone_output.status.success());
 
     let mut repo_dir = base_dir;
     repo_dir.push(REPO_DIRNAME);
 
-    // Init submodules
-    let backtrace_submod_output = Command::new(GIT_COMMAND)
-        .current_dir(&repo_dir)
-        .args(&["submodule", "update", "--init", "library/backtrace"])
-        .output()
-        .expect("Failed to git submodule init");
-    assert!(backtrace_submod_output.status.success());
-    let stdarch_submod_output = Command::new(GIT_COMMAND)
-        .current_dir(&repo_dir)
-        .args(&["submodule", "update", "--init", "library/stdarch"])
-        .output()
-        .expect("Failed to git submodule init");
-    assert!(stdarch_submod_output.status.success());
+    let repo = Repository::clone(REMOTE_URL, repo_dir)?;
+    repo_update_submodules(&repo)?;
+    Ok(repo)
+}
 
+fn repo_update_submodules(repo: &Repository) -> Result<()> {
+    let mut submodules = repo.submodules()?;
+    let cloned_submodules = vec!["library/backtrace", "library/stdarch"];
+    for submodule in &mut submodules {
+        let name = submodule.path().display().to_string();
+        if cloned_submodules.contains(&name.as_str()) {
+            println!("Updating submodule {}", name);
+            submodule.update(true, None)?;
+        }
+    }
     Ok(())
 }
 
-fn repo_update<'repo, P: AsRef<StdPath>>(repo_dir: P) -> Result<()> {
+fn repo_checkout_tag(repo: &Repository, tag: &Tag) -> Result<()> {
+    let tag_commit = tag.target().unwrap().peel_to_commit().unwrap();
+    repo_checkout_commit(repo, &tag_commit)
+}
+
+fn repo_checkout_commit(repo: &Repository, commit: &Commit) -> Result<()> {
+    let treeish = repo.revparse_single(&commit.id().to_string())?;
+    repo.checkout_tree(&treeish, None)?;
+    repo.set_head_detached(commit.id())?;
+    repo_update_submodules(repo)
+}
+
+fn repo_checkout_branch(repo: &Repository, branch_name: &str) -> Result<()> {
+    let branch = repo.revparse_single(branch_name)?;
+    repo.checkout_tree(&branch, None)?;
+    repo.set_head(&format!("refs/heads/{}", branch_name))?;
+    repo_update_submodules(repo)
+}
+
+fn repo_update(repo: &Repository) -> Result<()> {
+    let mut remote = repo.find_remote("origin")?;
     // Fetch including tags
-    let fetch_output = Command::new(GIT_COMMAND)
-        .current_dir(repo_dir.as_ref())
-        .args(&["fetch", "--tags"])
-        .output()
-        .expect("Failed to run git fetch");
-    assert!(fetch_output.status.success());
+    let mut fetch_options = FetchOptions::default();
+    fetch_options.download_tags(git2::AutotagOption::All);
+    remote.fetch(&[MAIN_BRANCH], Some(&mut fetch_options), None)?;
 
     // Merge remote branch
-    let merge_output = Command::new(GIT_COMMAND)
-        .current_dir(repo_dir.as_ref())
-        .args(&["merge", "--ff-only", REMOTE_REFSPEC])
-        .output()
-        .expect("Failed to git merge");
-    assert!(merge_output.status.success());
+    // Refer: https://github.com/libgit2/libgit2sharp/blob/5055fbda8bb319eba100f5e418d5beed534b83bc/LibGit2Sharp/Repository.cs#L1232
+    struct FetchHead {
+        _ref_name: String,
+        remote_url: String,
+        target_id: Oid,
+        _was_merged: bool,
+    }
+
+    let mut fetch_heads = Vec::new();
+    repo.fetchhead_foreach(|name, url, target, merged| {
+        fetch_heads.push(FetchHead {
+            _ref_name: name.into(),
+            remote_url: std::str::from_utf8(url).unwrap().into(),
+            target_id: *target,
+            _was_merged: merged,
+        });
+        true
+    })?;
+
+    let annotated_commits: Vec<_> = fetch_heads
+        .iter()
+        .map(|fetch_head| {
+            repo.annotated_commit_from_fetchhead(
+                MAIN_BRANCH,
+                &fetch_head.remote_url,
+                &fetch_head.target_id,
+            )
+            .unwrap()
+        })
+        .collect();
+
+    let annotated_commits: Vec<_> = annotated_commits.iter().collect();
+    repo.merge(&annotated_commits, None, None)?;
 
     // Update submodules
-    let backtrace_submod_output = Command::new(GIT_COMMAND)
-        .current_dir(&repo_dir)
-        .args(&["submodule", "update", "library/backtrace"])
-        .output()
-        .expect("Failed to git submodule init");
-    assert!(backtrace_submod_output.status.success());
-    let stdarch_submod_output = Command::new(GIT_COMMAND)
-        .current_dir(&repo_dir)
-        .args(&["submodule", "update", "library/stdarch"])
-        .output()
-        .expect("Failed to git submodule init");
-    assert!(stdarch_submod_output.status.success());
+    repo_update_submodules(repo)?;
     Ok(())
 }
 
-// Repo must be checked out to HEAD before calling this.
-fn repo_get_latest_tag<'repo>(repo: &'repo Repository) -> Result<Tag<'repo>> {
+fn repo_get_latest_tag(repo: &Repository) -> Result<Tag> {
     let mut tags = Vec::new();
     repo.tag_foreach(|id, _| {
         let tag = repo.find_tag(id).unwrap();
         tags.push(tag);
         true
     })?;
-
-    let latest = tags.into_iter().max_by_key(|tag| {
-        tag.target()
-            .unwrap()
-            .peel_to_commit()
-            .unwrap()
-            .time()
-            .seconds()
-    });
+    let latest = tags
+        .into_iter()
+        .max_by_key(|tag| tag.target().unwrap().peel_to_commit().unwrap().time());
 
     Ok(latest.unwrap())
 }
 
-fn repo_checkout_tag<P: AsRef<StdPath>>(repo_dir: P, tag_name: &str) -> Result<()> {
-    let checkout_output = Command::new(GIT_COMMAND)
-        .current_dir(repo_dir.as_ref())
-        .args(&["checkout", tag_name])
-        .output()
-        .expect("Failed to run git checkout");
-    assert!(checkout_output.status.success());
-    Ok(())
-}
-
-fn repo_checkout_branch<P: AsRef<StdPath>>(repo_dir: P, branch_name: &str) -> Result<()> {
-    let checkout_output = Command::new(GIT_COMMAND)
-        .current_dir(repo_dir.as_ref())
-        .args(&["checkout", branch_name])
-        .output()
-        .expect("Failed to run git checkout");
-    assert!(checkout_output.status.success());
-    Ok(())
-}
-
 pub struct StdRepo {
-    repo_path: PathBuf,
-    lib_path: PathBuf,
     crate_path: PathBuf,
+    repo: Repository,
 }
 
 impl StdRepo {
     pub fn new() -> Result<Self> {
-        let mut repo_dir =
-            home::home_dir().ok_or_else(|| Error::HomeDirNotFound("home dir not found"))?;
+        let mut repo_dir = home::home_dir().ok_or(Error::HomeDirNotFound("home dir not found"))?;
         repo_dir.push(BASE_DIRNAME);
         repo_dir.push(REPO_DIRNAME);
 
-        if !repo_dir.exists() {
-            repo_clone()?;
+        let repo = if !repo_dir.exists() {
+            repo_clone()?
+        } else {
+            Repository::open(&repo_dir)?
+        };
+
+        repo_checkout_branch(&repo, MAIN_BRANCH)?;
+        repo_update(&repo)?;
+
+        {
+            let latest_tag = repo_get_latest_tag(&repo)?;
+            repo_checkout_tag(&repo, &latest_tag)?;
         }
-
-        repo_checkout_branch(&repo_dir, MAIN_BRANCH)?;
-        repo_update(&repo_dir)?;
-
-        let repo = Repository::open(&repo_dir)?;
-        let latest_tag = repo_get_latest_tag(&repo)?;
-        repo_checkout_tag(&repo_dir, latest_tag.name().unwrap())?;
 
         let mut lib_path = repo_dir.clone();
         lib_path.push("library");
@@ -172,15 +168,7 @@ impl StdRepo {
         crate_path.push("library");
         crate_path.push("std");
 
-        Ok(Self {
-            repo_path: repo_dir,
-            lib_path,
-            crate_path,
-        })
-    }
-
-    pub fn lib_path(&self) -> &PathBuf {
-        &self.lib_path
+        Ok(Self { crate_path, repo })
     }
 
     pub fn crate_path(&self) -> &PathBuf {
@@ -190,6 +178,6 @@ impl StdRepo {
 
 impl Drop for StdRepo {
     fn drop(&mut self) {
-        repo_checkout_branch(&self.repo_path, MAIN_BRANCH).unwrap();
+        repo_checkout_branch(&self.repo, MAIN_BRANCH).unwrap();
     }
 }
